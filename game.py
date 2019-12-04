a02d0e570c6646817cc48e721ccf67e57c412fc6
snebotcifpfbmoll <snebot@cifpfbmoll.eu>
2019-12-04 15:57:11 +0100

    1r intento de juntarlo todo

--- game.py
+++ game.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 
 # Importar dependencias
+import os
+from sys import platform
 from tablero import mostrarTablero
 from coordenadas import *
-from movimientos import mover
+from movimientos import *
 
 # Constantes del programa
-E_BLANCO = 0
-E_NEGRO = 1
-D_NEGRA = 2
-D_BLANCA = 3
+MENU_WIDTH = 25
 
 # Tablero inicial (posicion de las damas)
 tablero = [
@@ -22,14 +21,56 @@ tablero = [
         [0, 3, 0, 3, 0, 3, 0, 3],
         [3, 0, 3, 0, 3, 0, 3, 0]]
 
+def clear():
+    if platform == "win32":
+        os.system("cls")
+    elif platform == "darwin" or platform == "linux" or platform == "linux2":
+        os.system("clear")
+
+# Mostrar las noramas del juego
+def normas():
+    clear()
+    print("%s" % ("=" * MENU_WIDTH))
+    print("Las damas se juegan entre dos personas en un tablero de ajedrez.Cada jugador dispone de 12 piezas de un mismo  color (blancas i  negras).")
+    print("Se juega por turnos alternos. Empieza a jugar el que juega con las blancas. En su  turno cada jugador mueve una pieza própia. Las piezas se mueven una posición adelante en diagonal.")
+    print("Cuando se come,las piezas saltan por encima de una pieza contraria en diagonal i van a parar a la casilla siguiente,que es obligatoria que este vacia.")
+    print("Las capturas se pueden encadenar,eso pasa si al comer una pieza y colocarte en la posición pertinente puedes volver a comer.")
+    print("Al llegar al otro extremo del tablero con una ficha,esta se convierte en DAMA,las damas pueden mover hacia adelante y hacia  atrás hacia las  cuatro direcciones en diagonal, una o varias casillas.")
+    print("%s" % ("=" * MENU_WIDTH))
+    input("PULSA CUALQUIER TECLA PARA VOLVER AL MENÚ")
+
+def menu():
+    clear()
+    print("%s" % ("=" * MENU_WIDTH))
+    print("=%s=" % ("MENÚ".center(MENU_WIDTH - 2)))
+    print("%s" % ("=" * MENU_WIDTH))
+    print("\t1) Normas")
+    print("\t2) Empezar partida")
+    print("\t3) Salir")
+    opcion = int(input("> "))
+    return opcion
+
 # Programa principal
-# TESTS
-mostrarTablero(tablero)
-# Preguntar por una cooredenadad
-print("\nIntroduce la coordenada de la ficha que quieres mover y la de destino. (<Origen>:<Destino>)")
-coord = input("> ")
-if ":" in coord:
-    c1, c2 = separarCoordenada(coord)
-    if comprobarCoordenada(c1) and comprobarCoordenada(c2):
-        mover(c1, c2, tablero)
-        mostrarTablero(tablero)
+opcion = 0
+while opcion != 3:
+    opcion = menu()
+    if opcion == 1:
+        normas()
+    elif opcion == 2:
+        # partida
+        while True:
+            clear()
+            mostrarTablero(tablero)
+            print("\nIntroduce la coordenada de la ficha que quieres mover y la de destino. (<Origen>:<Destino>)")
+            coord = input("> ")
+            if ":" in coord:
+                origen, destino = separarCoordenada(coord)
+                if comprobarCoordenada(origen) and comprobarCoordenada(destino):
+                    print(comprobarMovimiento(origen, destino, tablero))
+                    input()
+    elif opcion == 3:
+        # El bucle va a terminar
+        print("", end="")
+    else:
+        print("[Error] La opcion %d no es válida." % (opcion), end="")
+        input()
--- movimientos.py
+++ movimientos.py
@@ -3,7 +3,47 @@ from util import *
 from coordenadas import *
 
 def mover(origen, destino, tablero):
-    org_1, org_2 = indiceCoordenada(origen)
+    org_1, org_2 = indiceCoordenada(origen) #1 colmuna 2 fila.
     dst_1, dst_2 = indiceCoordenada(destino)
     tablero[dst_1][dst_2] = tablero[org_1][org_2]
     tablero[org_1][org_2] = E_NEGRO
+
+def comprobarColor(x,y,tablero):
+    origenC, origenF = indiceCoordenada(x)
+    destinoC, destinoF = indiceCoordenada(y)
+    comprobacion=False
+    if tablero[origenC][origenF]== D_BLANCA  and tablero[destinoC][destinoF]==D_NEGRA:
+        comprobacion=True
+        return comprobacion
+    if tablero[origenC][origenF]== D_NEGRA and tablero[destinoC][destinoF]==D_BLANCA :
+        comprobacion=True
+        return comprobacion
+
+def comprobarMovimiento(origen,destino,tablero):
+    origenC, origenF = indiceCoordenada(origen)
+    destinoC, destinoF = indiceCoordenada(destino)
+
+    if tablero[origenC,origenF]==3:
+        if destinoC==(origenC+2):
+            fichaIntermediaC=(destinoC-1)
+            fichaIntermediaF=(destinoF+1)
+            fichaIntermedia=(fichaIntermediaC,fichaIntermediaF)
+            return comprobarColor(origen,fichaIntermedia,tablero)
+
+        if destinoC==(origenC-2):
+            fichaIntermediaC=(destinoC+1)
+            fichaIntermediaF=(destinoF+1)
+            return comprobarColor(origen,fichaIntermedia,tablero)
+
+    if tablero[origenC,origenF]==2:
+        if destinoC==(origenC+2):
+            fichaIntermediaC=(destinoC-1)
+            fichaIntermediaF=(destinoF-1)
+            fichaIntermedia=(fichaIntermediaC,fichaIntermediaF)
+            return comprobarColor(origen,fichaIntermedia,tablero)
+
+        if destinoC==(origenC-2):
+            fichaIntermediaC=(destinoC+1)
+            fichaIntermediaF=(destinoF-1)
+            comprobacion=comprobarColor(origen,fichaIntermedia,tablero)
+            return comprobarColor(origen,fichaIntermedia,tablero)
